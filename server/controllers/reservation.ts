93a1884fef2ba5c125651384f0a7ca18f5cfe82a
Raymond <raymondtdev@gmail.com>
2026-09-09 21:40:40 +0200

    reservation creation controller

--- server/controllers/reservation.ts
+++ server/controllers/reservation.ts
@@ -1,12 +1,66 @@
 import { Request, Response } from "express";
 import Reservation from "../models/Reservation";
-import { tables, timeSlots } from "../utils/data";
+import { timeSlots } from "../utils/data";
+
+interface ReservationType {
+  user: string;
+  table: {
+    name: string;
+    guests: number;
+  };
+  timeSlot: string;
+  date: Date;
+  status: "pending" | "confirmed" | "cancelled";
+  createdAt: Date;
+}
 
 export const checkAvailability = async (req: Request, res: Response) => {
   try {
+
+    const { date, table } = req.body as { date: string; table: string };
+    const reservations: ReservationType[] = await Reservation.find({ date, "table.name": table });
+
+    const reservedSlots = reservations.map(reservation => reservation.timeSlot);
+    const availableSlots = timeSlots.filter(slot => !reservedSlots.includes(slot));
+
+    res.status(200).json({ availableSlots });
     
   } catch (error) {
     console.error("Error checking availability:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+export const createResevation = async (req: Request, res: Response) => {
+  try {
+    const { userInfo, date, table, timeSlot } = req.body as {
+      userInfo: {
+        name: string;
+        surname: string;
+        email: string;
+        phone: string;
+        ocassion?: string;
+        specialRequest?: string;
+      },
+      date: string;
+      table: {
+        name: string;
+        guests: number;
+      };
+      timeSlot: string;
+    }
+
+    const newReservation = new Reservation({
+      user: userInfo,
+      table,
+      timeSlot,
+      date
+    });
+
+    await newReservation.save();
+    res.status(201).json({ message: "Reservation created successfully", reservation: newReservation });
+  } catch (error) {
+    console.error("Error creating reservation:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 }
--- server/models/Reservation.ts
+++ server/models/Reservation.ts
@@ -3,12 +3,17 @@ import { Schema, Model } from "mongoose";
 const ReservationSchema: Schema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   table: {
-    name: { type: String, required: true },
+    name: { 
+      type: String,
+      enum: ["A1", "A2", "A3", "B1", "B2", "B3"],
+      required: true
+    },
     guests: { type: Number, required: true }
   },
   timeSlot: { type: String, required: true },
   date: { type: Date, required: true },
   status: { type: String, enum: ["pending", "confirmed", "cancelled"], default: "pending" },
+  createdAt: { type: Date, default: Date.now },
 });
 
 const Reservation = new Model("Reservation", ReservationSchema);
